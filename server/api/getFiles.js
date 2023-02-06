7786181b057338d74fb83b8bb428acd5e6c5bc36
Agil Setiawan <agilsetiawan772@gmail.com>
2023-02-06 08:34:00 +0700

    Delete getFiles.js

--- server/api/getFiles.js
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const { apiSecret } = useRuntimeConfig()
-    return $fetch('https://panel.dapurlestari.id/api/upload/files', {
-        headers: {
-            Authorization: `Bearer ${apiSecret}`
-        }
-    })
-})
