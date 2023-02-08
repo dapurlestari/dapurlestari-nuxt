18cf09b557f14110648124b088c4eb65c340336f
Agil Setiawan <agilsetiawan772@gmail.com>
2023-02-08 19:59:21 +0700

    Create FooterSection.vue

---
+++ components/FooterSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <footer class="footer footer-center p-10 bg-gray-100 text-base-content rounded">
+            <div class="grid grid-flow-col gap-4">
+                <a class="link link-hover">About Us</a> 
+                <a class="link link-hover">Contact</a> 
+                <a class="link link-hover">Career</a> 
+                <a class="link link-hover">Privacy Policy</a>
+                <a class="link link-hover">Terms of Use</a>
+            </div> 
+            <div>
+                <div class="grid grid-flow-col gap-4">
+                    <a v-for="item in $socials" :key="item.id" :href="item.attributes.link" target="_blank" class="">
+                        <i :class="item.attributes.icon+' text-lg hover:text-secondary-500'"></i>
+                    </a>
+                </div>
+            </div> 
+            <div>
+                <p>{{ $myConfig.copyright }}</p>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+    const { $myConfig, $socials } = useNuxtApp()
+    console.log($socials);
+</script>
