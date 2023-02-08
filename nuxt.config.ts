bdd606364cb3eee4dbecca0a1b316f7c77a29455
Agil Setiawan <agilsetiawan772@gmail.com>
2023-02-08 20:01:03 +0700

    Add google map plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  // build: { transpile: ["@fawmi/vue-google-maps"] },
+  build: {
+    transpile: [
+      '@fawmi/vue-google-maps', 
+      '@googlemaps/markerclusterer',
+      "fast-deep-equal",
+    ]
+  },
   app: {
     head: {
       title: 'Dapur Lestari',
@@ -9,7 +19,12 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel:"stylesheet", type:"text/css", href:'https://cdn.lineicons.com/4.0/lineicons.css' }
-      ]
+      ],
+      /* script: [
+        {
+          src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places&callback=Function.prototype`
+        }
+      ] */
     },
   },
   runtimeConfig: {
@@ -18,7 +33,9 @@ export default defineNuxtConfig({
     // Keys within public are also exposed client-side
     public: {
       baseURL: process.env.NUXT_BASE_URL,
-      apiBase: process.env.NUXT_PUBLIC_API_BASE
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+      mapAPIKey: process.env.NUXT_MAP_API_KEY,
+      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY
     }
   }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -62,6 +62,20 @@ module.exports = {
         sans: ['Montserrat'],
         serif: ['Playfair Display'],
       },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     }
   },
   plugins: [require("daisyui")],
