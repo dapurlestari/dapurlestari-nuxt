d58d0698bcfdcf676de52235d763c1acc40b70d3
Agil Setiawan <agilsetiawan772@gmail.com>
2023-02-08 19:59:50 +0700

    Sync social API

--- plugins/customConfig.js
+++ plugins/customConfig.js
@@ -1,9 +1,10 @@
 export default defineNuxtPlugin(async (nuxtApp) => {
     console.log(`App Name: ${nuxtApp.globalName}`);
     const { data } = await useFetch('/api/config/get')
+    const { data: social } = await useFetch('/api/socials/get')
     
     const config = useRuntimeConfig()
-    // console.log(data.value)
+    // console.log(social)
     const configData = data.value.data
     console.log(configData.attributes.logo.data.attributes.url)
     const logoURL = config.public.baseURL+configData.attributes.logo.data.attributes.url
@@ -13,7 +14,14 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     return {
         provide: {
             myConfig: configData.attributes,
-            logo: logoURL
+            socials: social.value.data,
+            logo: logoURL,
+            formatPrice: (number) => {
+                return new Intl.NumberFormat('id-ID', {
+                    maximumSignificantDigits: 3,
+                    currency: 'IDR',
+                }).format(number)
+            }
         }
     }
 })
