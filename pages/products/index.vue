88350e008299c701fcc71768051f7ac32e488213
Agil Setiawan <agilsetiawan772@gmail.com>
2023-02-09 22:47:30 +0700

    Add ads banners on product page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,30 +3,88 @@
         <template #title>Products</template>
 
         <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <div class="w-full grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                <div v-for="product in products">
-                    <ProductCard :product="product" />
+
+            <section>
+                <div class="grid grid-rows-6 grid-flow-col gap-4 h-64">
+                    <div class="row-span-6 col-span-3 bg-gray-200 rounded-lg">
+                        &nbsp;&nbsp;ads
+                    </div>
+                    
+                    <div class="row-span-3 col-span-3 bg-gray-200 rounded-lg">
+                        &nbsp;&nbsp;ads
+                    </div>
+
+                    <div class="row-span-3 col-span-3 bg-gray-200 rounded-lg">
+                        &nbsp;&nbsp;ads
+                    </div>
+                </div>
+            </section>
+
+            <div class="divider"></div> 
+
+            <section class="mt-5">
+                <div class="flex">
+                    <div class="flex-1 w-14 h-14 relative">
+                        <input 
+                            type="text" 
+                            name="" 
+                            id="" 
+                            placeholder="Kamu mau cari apa..."
+                            class="w-full h-full p-3 bg-gray-100 border border-gray-200 caret-secondary-500 text-black rounded-md">
+                        <i class="lni lni-search-alt absolute right-0 top-0 text-2xl py-3 px-6"></i>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+
+            </section>
+
+            <div class="w-full grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mt-5">
+                <div v-for="product in products" :key="product.id">
+                    <div>
+                        <NuxtLink :to="'/products/'+product.attributes.slug" class="group">
+                            <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                                <img 
+                                    :src="baseURL+product.attributes.images.data[0].attributes.url" 
+                                    :alt="product.attributes.images.data[0].alternativeText" 
+                                    class="h-60 w-full object-cover object-center group-hover:opacity-75" />
+                            </div>
+                            <h3 class="mt-4 text-secondary-500">{{ product.attributes.name }}</h3>
+                            <p class="mt-1 text-lg font-medium text-gray-900">{{ $formatPrice(product.attributes.price) }}</p>
+                        </NuxtLink>
+                    </div>
+                    <button class="rounded-lg bg-secondary-500 hover:bg-secondary-600 text-white p-2 w-full text-sm mt-2">
+                        Tambah ke keranjang
+                    </button>
                 </div>
             </div>
+
+            <section class="flex justify-center mt-20">
+                <div class="btn-group grid grid-cols-2">
+                    <button class="btn btn-outline">Previous page</button>
+                    <button class="btn btn-outline">Next</button>
+                </div>
+            </section>
         </div>
     </NuxtLayout>
 </template>
 
 <script setup>
-    /* definePageMeta({
-        layout: 'products'
-    }) */
+    const { $formatPrice } = useNuxtApp()
+    const config = useRuntimeConfig()
+    const baseURL = config.public.baseURL
 
     const {data: response} = await useFetch('/api/products/get')
     const products = response.value.data
-    console.log(products);
-    useHead({
+    // console.log(products);
+    /* useHead({
       title: 'Dapur Lestari | Products',
       meta: [
         {name: 'description', content: 'Dapur Lestari Products'},
         {name: 'keywords', content: 'dapur, lestari, produk, kukis'},
       ],
-    })
+    }) */
 </script>
 
 <style scoped>
