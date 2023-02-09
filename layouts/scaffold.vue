1b9d02a883c8de50a0fdb18633295930a2010312
Agil Setiawan <agilsetiawan772@gmail.com>
2023-02-09 22:47:42 +0700

    Update back button on scaffold layout

--- layouts/scaffold.vue
+++ layouts/scaffold.vue
@@ -2,9 +2,9 @@
     <div>
         <div class="navbar min-h-0 fixed z-50 bg-white shadow-lg">
             <div class="navbar-start">
-                <NuxtLink to="/">
+                <button @click="router.back">
                     <i class="lni lni-chevron-left p-2"></i>
-                </NuxtLink>
+                </button>
                 <span class="normal-case text-md pl-2">
                     <slot name="title">Dapur Lestari</slot>
                 </span>
@@ -15,7 +15,11 @@
 </template>
 
 <script setup>
-
+    const router = useRouter()
+    // router.back()
+    // const paths = route.path.split('/')
+    // const prevPath = paths[paths.length-2]
+    // console.log(route.redirectedFrom.path);
 </script>
 
 <style scoped>
