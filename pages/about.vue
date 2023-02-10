65e8569e81205ccd2a2c6b20f24462e03348d8a5
Agil Setiawan <agilsetiawan772@gmail.com>
2023-02-10 17:02:22 +0700

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,14 +2,17 @@
     <NuxtLayout name="scaffold">
         <template #title>{{ about.title }}</template>
 
-        <div class="flex container">
-            <h2>{{ about.title }}</h2>
-            <p>{{ about.content }}</p>
+        <div class="flex container mx-auto px-4 pt-16">
+            <article class="prose lg:prose-xl">
+                <h2>{{ about.title }}</h2>
+                <div v-html="$mdit.render(about.content)" class="text-sm text-gray-700"></div>
+            </article>
         </div>
     </NuxtLayout>
 </template>
 
 <script setup>
+    const { $mdit } = useNuxtApp()
     const { data } = await useFetch('/api/pages/about')
     const about = data.value.data.attributes.contentful
 </script>
