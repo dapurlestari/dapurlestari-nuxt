3ce949a4f00e387401c8e3acd373a510d93e1c7e
Agil Setiawan <agilsetiawan772@gmail.com>
2023-02-10 17:03:35 +0700

    Update privacy-policy.vue

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -2,14 +2,17 @@
     <NuxtLayout name="scaffold">
         <template #title>{{ privacyPolicy.title }}</template>
 
-        <div class="flex container">
-            <h2>{{ privacyPolicy.title }}</h2>
-            <p>{{ privacyPolicy.content }}</p>
+        <div class="flex container mx-auto px-4 pt-16">
+            <article class="prose lg:prose-xl">
+                <h2>{{ privacyPolicy.title }}</h2>
+                <div v-html="$mdit.render(privacyPolicy.content)" class="text-sm text-gray-700"></div>
+            </article>
         </div>
     </NuxtLayout>
 </template>
 
 <script setup>
+    const { $mdit } = useNuxtApp()
     const { data } = await useFetch('/api/pages/privacyPolicy')
     const privacyPolicy = data.value.data.attributes.contentful
 </script>
