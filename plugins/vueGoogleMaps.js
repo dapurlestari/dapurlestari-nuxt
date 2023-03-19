ccde3db5464007061616f4cc9ff6bae307eb5536
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-19 09:58:36 +0700

    Update google map plugin

--- plugins/vueGoogleMaps.js
+++ plugins/vueGoogleMaps.js
@@ -1,11 +1,11 @@
-import VueGoogleMaps from "@fawmi/vue-google-maps"
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 
 // bug fixed: https://github.com/fawmi/vue-google-maps/issues/152#issuecomment-1264630070
 export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig().public
+  const config = useRuntimeConfig().public;
   nuxtApp.vueApp.use(VueGoogleMaps, {
     load: {
       key: config.GOOGLE_MAPS_API_KEY,
     },
-  })
-})
+  });
+});
