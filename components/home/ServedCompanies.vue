f46c6bb5f73f56570a5939a465603dbb07267ad4
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-19 10:02:00 +0700

    Update ServedCompanies.vue

--- components/home/ServedCompanies.vue
+++ components/home/ServedCompanies.vue
@@ -1,73 +1,75 @@
 <template>
-    <div>
-        <section class="flex justify-center mt-32">
-            <div class="text-slate-900 text-center justify-center">
-                <h2 class="font-bold text-2xl text-primary-500">{{ servedCompanies.title }}</h2>
-                <p class="font-light tracking-wide text-xl my-3 mx-auto max-w-2xl font-serif px-5">
-                    {{ servedCompanies.subtitle }}
-                </p>
+  <div>
+    <section class="flex justify-center mt-32">
+      <div class="text-slate-900 text-center justify-center">
+        <h2 class="font-bold text-3xl text-primary-500">
+          {{ servedCompanies.title }}
+        </h2>
+        <p class="tracking-wide text-lg my-3 mx-auto max-w-2xl px-5">
+          {{ servedCompanies.subtitle }}
+        </p>
 
-                <article id="company-logo" class="flex overflow-hidden whitespace-nowrap mt-10">
-                    <div class="relative">
-                        <ul class="marquee flex list-none pl-0">
-                            <li v-for="item in servedCompanies.companies">
-                                <img 
-                                :src="baseURL+item.logo.data.attributes.url" 
-                                :alt="item.name"
-                                class="max-h-28 mr-2">
-                            </li>
-                        </ul>
-                        <ul class="marquee2 flex list-none pl-0 absolute top-0">
-                            <li v-for="item in servedCompanies.companies">
-                                <img 
-                                :src="baseURL+item.logo.data.attributes.url" 
-                                :alt="item.name"
-                                class="max-h-28 mr-2">
-                            </li>
-                        </ul>
-                    </div>
-                </article>
-            </div>
-        </section>
-    </div>
+        <article
+          id="company-logo"
+          class="flex overflow-hidden whitespace-nowrap mt-10">
+          <div class="relative">
+            <ul class="marquee flex list-none pl-0">
+              <li v-for="item in servedCompanies.companies">
+                <img
+                  :src="baseURL + item.logo.data.attributes.url"
+                  :alt="item.name"
+                  class="max-h-28 mr-2" />
+              </li>
+            </ul>
+            <ul class="marquee2 flex list-none pl-0 absolute top-0">
+              <li v-for="item in servedCompanies.companies">
+                <img
+                  :src="baseURL + item.logo.data.attributes.url"
+                  :alt="item.name"
+                  class="max-h-28 mr-2" />
+              </li>
+            </ul>
+          </div>
+        </article>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup>
-    const config = useRuntimeConfig()
-    const { servedCompanies } = defineProps(['servedCompanies'])
-    const baseURL = config.public.baseURL
-
+const config = useRuntimeConfig();
+const { servedCompanies } = defineProps(["servedCompanies"]);
+const baseURL = config.public.baseURL;
 </script>
 
 <style scoped>
-    .marquee {
-        animation: marquee 5s linear infinite;
-    }
+.marquee {
+  animation: marquee 5s linear infinite;
+}
 
-    .marquee2 {
-        animation: marquee2 5s linear infinite;
-    }
+.marquee2 {
+  animation: marquee2 5s linear infinite;
+}
 
-    #company-logo ul li {
-        margin: 8px;
-    }
+#company-logo ul li {
+  margin: 8px;
+}
 
-    @keyframes marquee {
-        from {
-            transform: translateX(0%);
-        }
-        to {
-            transform: translateX(-100%);
-        }
-    }
+@keyframes marquee {
+  from {
+    transform: translateX(0%);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+}
 
-    @keyframes marquee2 {
-        from {
-            transform: translateX(100%);
-        }
-        to {
-            transform: translateX(0%);
-        }
-    }
-
+@keyframes marquee2 {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+}
 </style>
