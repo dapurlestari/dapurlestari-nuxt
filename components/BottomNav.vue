b166d797d90c350f1768076348257a75857bfec8
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-20 19:22:18 +0700

    Update BottomNav.vue

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -34,7 +34,7 @@
 
       <!-- Bottom Navbar for mobile screen -->
       <div
-        class="navbar light-mode bg-base-200 z-10 fixed bottom-0 pt-1 m-0 inline-block md:hidden rounded-t-2xl">
+        class="navbar light-mode bg-base-200 z-10 fixed bottom-0 pt-1 m-0 inline-block md:hidden rounded-t-2xl shadow-[0_-2px_20px_5px_rgba(0,0,0,0.2)]">
         <div class="navbar-center w-full h-full">
           <ul
             class="menu menu-horizontal items-center justify-evenly w-full flex p-0 h-full">
