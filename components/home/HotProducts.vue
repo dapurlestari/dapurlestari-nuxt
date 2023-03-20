d50b29e46bbf2f814068cfeedb3f8c47196a5ee8
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-20 17:02:56 +0700

    Update HotProducts.vue

--- components/home/HotProducts.vue
+++ components/home/HotProducts.vue
@@ -7,11 +7,41 @@
       <p class="tracking-wide my-3 mx-auto max-w-3xl">
         {{ hotProducts.caption }}
       </p>
-      <a
-        href="#"
-        class="inline-block mt-10 rounded-md border border-transparent bg-secondary-500 py-3 px-8 text-center font-medium text-white hover:bg-secondary-600">
-        Belanja Sekarang
-      </a>
+
+      <div
+        class="grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-5 mt-10 mx-0 md:mx-20">
+        <div
+          v-for="product in hotProducts.products.data"
+          class="group space-y-2">
+          <div class="relative">
+            <div class="overflow-hidden rounded-2xl bg-gray-200">
+              <img
+                :src="
+                  baseURL +
+                  product.attributes.images.data[0].attributes.formats.medium
+                    .url
+                "
+                :alt="product.attributes.name"
+                class="aspect-1 object-cover object-center group-hover:opacity-75" />
+            </div>
+            <div class="absolute bottom-0 left-0 p-4 w-full">
+              <h3
+                class="text-lg font-semibold text-white line-clamp-1 drop-shadow-[0px_4px_7px_rgba(0,0,0,0.85)]">
+                {{ product.attributes.name }}
+              </h3>
+              <p
+                class="mt-1 text-white line-clamp-1 drop-shadow-[0px_2px_7px_rgba(0,0,0,1)]">
+                {{ $formatPrice(product.attributes.price) }}
+              </p>
+            </div>
+          </div>
+          <NuxtLink
+            :to="`/store/${product.attributes.slug}`"
+            class="btn btn-primary rounded-lg text-white bg-secondary-500 hover:bg-secondary-600 border-transparent hover:border-transparent px-5 font-bold w-full capitalize">
+            Beli Sekarang
+          </NuxtLink>
+        </div>
+      </div>
     </div>
   </section>
 </template>
