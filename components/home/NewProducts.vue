f43ae5c64a37f84c155230c0fc7ca26f9301e30f
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-20 17:02:50 +0700

    Update NewProducts.vue

--- components/home/NewProducts.vue
+++ components/home/NewProducts.vue
@@ -17,25 +17,27 @@
             <div class="overflow-hidden rounded-2xl bg-gray-200">
               <img
                 :src="
-                  baseURL + product.attributes.images.data[0].attributes.url
+                  baseURL +
+                  product.attributes.images.data[0].attributes.formats.medium
+                    .url
                 "
                 :alt="product.attributes.name"
                 class="aspect-1 object-cover object-center group-hover:opacity-75" />
             </div>
             <div class="absolute bottom-0 left-0 p-4 w-full">
               <h3
-                class="text-xl font-semibold text-white line-clamp-1 drop-shadow-[2px_4px_7px_rgba(0,0,0,0.7)]">
+                class="text-lg font-semibold text-white line-clamp-1 drop-shadow-[0px_4px_7px_rgba(0,0,0,0.85)]">
                 {{ product.attributes.name }}
               </h3>
               <p
-                class="mt-1 text-lg text-white line-clamp-1 drop-shadow-[2px_4px_7px_rgba(0,0,0,0.7)]">
+                class="mt-1 text-white line-clamp-1 drop-shadow-[0px_2px_7px_rgba(0,0,0,1)]">
                 {{ $formatPrice(product.attributes.price) }}
               </p>
             </div>
           </div>
           <NuxtLink
-            :to="'/store/' + product.attributes.slug"
-            class="btn rounded-xl bg-secondary-400 hover:bg-secondary-500 border-transparent hover:border-transparent px-5 text-primary-800 font-bold w-full">
+            :to="`/store/${product.attributes.slug}`"
+            class="btn btn-primary rounded-lg text-white bg-secondary-500 hover:bg-secondary-600 border-transparent hover:border-transparent px-5 font-bold w-full capitalize">
             Beli Sekarang
           </NuxtLink>
         </div>
