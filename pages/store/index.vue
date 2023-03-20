ad72bae3e742b5b032bfdd8dfc24f49dd1adcb23
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-20 19:22:09 +0700

    Update index.vue

--- pages/store/index.vue
+++ pages/store/index.vue
@@ -76,7 +76,7 @@
       </div>
     </div>
 
-    <section class="flex justify-center mt-20">
+    <section class="flex justify-center mt-20 mb-8">
       <div class="btn-group grid grid-cols-2">
         <button class="btn btn-outline">Previous page</button>
         <button class="btn btn-outline">Next</button>
