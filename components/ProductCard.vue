984c336faa9df79e7ead12bdc6e3470f6990e3a9
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-21 15:12:59 +0700

    Update ProductCard.vue

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,29 +1,41 @@
 <template>
-  <NuxtLink :to="`/products/${productAttr.slug}`" class="group">
-    <div class="card card-compact bg-base-100 shadow-xl">
-      <figure>
-        <img :src="imageURL" :alt="productAttr.name" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">{{ productAttr.name }}</h2>
-        <p>{{ productAttr.price }}</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Pesan Sekarang</button>
-        </div>
+  <div class="group space-y-2">
+    <div class="relative">
+      <div class="overflow-hidden rounded-2xl bg-gray-200">
+        <NuxtLink :to="`/store/${productAttr.slug}`">
+          <img
+            :src="imageURL"
+            :alt="productAttr.name"
+            class="aspect-1 sm:aspect-2 md:aspect-1 object-cover object-center group-hover:opacity-75 w-full" />
+        </NuxtLink>
+      </div>
+      <div
+        class="absolute bottom-0 left-0 p-4 w-full rounded-b-2xl bg-gradient-to-t from-secondary-400 to-transparent drop-shadow-lg">
+        <h3
+          class="text-lg font-bold text-white line-clamp-1 drop-shadow-[0px_0px_2px_rgba(0,0,0,0.5)]">
+          {{ productAttr.name }}
+        </h3>
+        <p
+          class="mt-1 font-semibold text-white line-clamp-1 drop-shadow-[0px_0px_2px_rgba(0,0,0,0.5)]">
+          {{ $formatPrice(productAttr.price) }}
+        </p>
       </div>
     </div>
-  </NuxtLink>
+    <NuxtLink
+      :to="`/store/${productAttr.slug}`"
+      class="btn btn-primary rounded-lg text-secondary-900 bg-secondary-400 hover:bg-secondary-500 border-transparent hover:border-transparent px-5 font-bold w-full capitalize drop-shadow-lg">
+      Beli Sekarang
+    </NuxtLink>
+  </div>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
 const { product } = defineProps(["product"]);
-const id = product.id;
 let productAttr = product.attributes;
-productAttr.id = id;
+const firstImage = productAttr.images.data[0].attributes;
 const imageURL =
-  config.public.baseURL +
-  productAttr.images.data[0].attributes.formats.medium.url;
+  config.public.baseURL + (firstImage.formats.medium ?? firstImage).url;
 </script>
 
 <style scoped></style>
