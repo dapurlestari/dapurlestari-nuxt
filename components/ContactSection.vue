4b33ffd173ca43f2ea7c6e66b8f9d3919cc3ada0
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-21 10:34:56 +0700

    Update contact section

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-hidden bg-white py-24 sm:py-32">
+  <div class="overflow-hidden bg-gray-50 py-20">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div
         class="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
@@ -25,10 +25,10 @@
                 {{ " " }}
                 <dd class="inline text-gray-900">
                   <a
-                    :href="$myConfig.whatsapp_link"
+                    :href="app.$myConfig.whatsapp_link"
                     target="_blank"
                     class="underline"
-                    >{{ $myConfig.phone }}</a
+                    >{{ phone }}</a
                   >
                 </dd>
               </div>
@@ -41,10 +41,10 @@
                 {{ " " }}
                 <dd class="inline text-gray-900">
                   <a
-                    :href="$myConfig.email"
+                    :href="app.$myConfig.email"
                     target="_blank"
                     class="underline"
-                    >{{ $myConfig.email }}</a
+                    >{{ app.$myConfig.email }}</a
                   >
                 </dd>
               </div>
@@ -55,7 +55,9 @@
                   Alamat.
                 </dt>
                 {{ " " }}
-                <dd class="inline text-gray-900">{{ $myConfig.address }}</dd>
+                <dd class="inline text-gray-900">
+                  {{ app.$myConfig.address }}
+                </dd>
               </div>
             </dl>
           </div>
@@ -64,7 +66,7 @@
         <!-- <img src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442" /> -->
 
         <GMapMap
-          class="rounded-xl shadow-xl w-[340px] md:w-[660px] h-[450px] md:h-[420px] m-auto"
+          class="rounded-xl shadow-xl w-full h-[450px] md:h-[420px] m-auto"
           :center="location"
           :zoom="map.zoom"
           :options="{
@@ -87,15 +89,16 @@
 </template>
 
 <script setup>
-const { $myConfig } = useNuxtApp();
+const app = useNuxtApp();
 // const config = useRuntimeConfig()
 const contact = {
   title: "Segera hubungi kami!",
   subtitle: "Punya pertanyaan?",
 };
 
-const map = $myConfig.map;
-console.log(map);
+const phone = app.$formatPhoneNumberID(app.$myConfig.phone);
+const map = app.$myConfig.map;
+// console.log(map);
 const markers = map.markers;
 const defaultMarker = markers[0];
 const location = {
