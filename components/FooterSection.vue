ad3ba29de138ab3fa1a02898f1da9d3e1ce16a25
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-21 07:31:00 +0700

    Update FooterSection.vue

--- components/FooterSection.vue
+++ components/FooterSection.vue
@@ -1,28 +1,36 @@
 <template>
-    <div>
-        <footer class="footer footer-center p-10 bg-gray-100 text-base-content rounded">
-            <div class="grid grid-flow-col gap-4">
-                <!-- <a class="link link-hover">About Us</a> 
-                <a class="link link-hover">Contact</a> 
-                <a class="link link-hover">Career</a>  -->
-                <NuxtLink to="/privacy-policy" class="link link-hover">Privacy Policy</NuxtLink>
-                <NuxtLink to="/terms-services" class="link link-hover">Terms and Services</NuxtLink>
-            </div> 
-            <div>
-                <div class="grid grid-flow-col gap-4">
-                    <a v-for="item in $socials" :key="item.id" :href="item.attributes.link" :aria-label="item.attributes.name" target="_blank" class="">
-                        <i :class="item.attributes.icon+' text-lg hover:text-secondary-500'"></i>
-                    </a>
-                </div>
-            </div> 
-            <div class="mb-12">
-                <p class="text-sm">{{ $myConfig.copyright }}</p>
-            </div>
-        </footer>
-    </div>
+  <div>
+    <footer
+      class="footer footer-center p-10 bg-gray-100 text-base-content rounded">
+      <div class="grid grid-flow-col gap-4">
+        <NuxtLink to="/privacy-policy" class="link link-hover">
+          Privacy Policy
+        </NuxtLink>
+        <NuxtLink to="/terms-services" class="link link-hover">
+          Terms and Services
+        </NuxtLink>
+      </div>
+      <div>
+        <div class="grid grid-flow-col gap-4">
+          <a
+            v-for="item in $socials"
+            :key="item.id"
+            :href="item.attributes.link"
+            :aria-label="item.attributes.name"
+            target="_blank"
+            class="">
+            <i
+              :class="
+                item.attributes.icon + ' text-lg hover:text-secondary-500'
+              "></i>
+          </a>
+        </div>
+      </div>
+      <div class="mb-12">
+        <p class="text-sm">{{ $myConfig.copyright }}</p>
+      </div>
+    </footer>
+  </div>
 </template>
 
-<script setup>
-    const { $myConfig, $socials } = useNuxtApp()
-    console.log($socials);
-</script>
+<script setup></script>
