fd7754ab8220fbca0ecfaf0120dca28f12d4b048
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-21 10:33:35 +0700

    Fix home lazy load

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,49 @@
   <div>
     <HomeLandingHero :first-slideshow="homeData.slideshows[0]" />
 
-    <HomeHotProducts :hot-products="homeData.hot_products" />
+    <div v-if="pending" class=""></div>
+    <HomeHotProducts v-else :hot-products="homeData.hot_products" />
 
+    <div v-if="pending" class=""></div>
     <HomeRecomSection
+      v-else
       :recommendations="homeData.recommendations"
       :recommendation="homeData.recommended_products" />
 
-    <HomeNewProducts :new-products="homeData.new_products" />
+    <div v-if="pending" class=""></div>
+    <HomeNewProducts v-else :new-products="homeData.new_products" />
 
     <!-- marquee by https://jackwhiting.co.uk/posts/creating-a-marquee-with-tailwind/ -->
-    <HomeServedCompanies :served-companies="homeData.served_companies" />
+    <div v-if="pending" class=""></div>
+    <HomeServedCompanies v-else :served-companies="homeData.served_companies" />
 
-    <ContactSection />
+    <div v-if="pending" class=""></div>
+    <ContactSection v-else />
+
+    <div v-if="pending" class="h-[400px] bg-white"></div>
   </div>
 </template>
 
 <script setup>
-const { data } = await useFetch("/api/pages/home");
-const homeData = data.value.data.attributes;
+const app = useNuxtApp();
+const defaultImage = app.$placeholderImageURL("Featured Main Image", 600, 450);
+var homeData = {
+  slideshows: [
+    {
+      subtitle: "...",
+      image: {
+        data: {
+          attributes: {
+            url: defaultImage,
+          },
+        },
+      },
+    },
+  ],
+};
+
+const { pending, data } = await useLazyFetch("/api/pages/home");
+homeData = data.value.data.attributes;
 </script>
 
 <style scoped></style>
