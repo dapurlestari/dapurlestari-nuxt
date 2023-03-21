d85177721750a993413a58a9da168fba75d9a31e
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-21 15:13:39 +0700

    Update ScrollToTop.vue

--- components/ScrollToTop.vue
+++ components/ScrollToTop.vue
@@ -1,20 +1,11 @@
 <template>
   <button
     @click="scrollToTop"
-    class="fixed bottom-20 right-4 bg-gray-700 text-white rounded-full p-3 shadow-md hover:bg-gray-600 transition-all duration-200 md:bottom-8 md:right-8 md:p-4 md:text-lg md:shadow-xl"
+    type="button"
+    class="btn btn-circle fixed bottom-20 right-4 bg-gray-700 text-white rounded-full shadow-md hover:bg-gray-600 transition-all duration-200 md:bottom-8 md:right-8 md:text-lg md:shadow-xl"
+    title="Scroll to Top"
     :class="{ 'opacity-0': !showButton }">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor">
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M5 10l7-7m0 0l7 7m-7-7v18" />
-    </svg>
+    <i class="lni lni-arrow-up"></i>
   </button>
 </template>
 
