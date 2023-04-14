1565c5b06d622080470b7ea364d1eeb3c485706c
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-14 10:18:44 +0700

    Update ServedCompanies.vue

--- components/home/ServedCompanies.vue
+++ components/home/ServedCompanies.vue
@@ -12,21 +12,18 @@
           id="company-logo"
           class="flex flex-row flex-wrap justify-center max-w-7xl gap-4 mx-auto px-4 pt-8">
           <div
-            v-for="item in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
+            v-for="item in servedCompanies.companies"
             :key="item"
             class="p-2 rounded-xl">
-            <a :href="servedCompanies.companies[0].link ?? ''" target="_blank">
+            <a :href="item.link ?? ''" target="_blank">
               <img
-                :src="
-                  baseURL +
-                  servedCompanies.companies[0].logo.data.attributes.url
-                "
-                :alt="servedCompanies.companies[0].name"
+                :src="baseURL + item.logo.data.attributes.url"
+                :alt="item.name"
                 width="158"
                 height="48"
                 class="max-h-24 mx-auto object-contain" />
             </a>
-            <span>{{ servedCompanies.companies[0].description }}</span>
+            <span>{{ item.description }}</span>
           </div>
         </article>
       </div>
