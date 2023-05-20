7efd9a179f1af862919c36c11f0fc099f5c14267
Agil Setiawan <agilsetiawan772@gmail.com>
2023-05-20 23:58:03 +0700

    Update products

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="group space-y-2">
     <div class="relative">
-      <div class="overflow-hidden rounded-2xl bg-gray-200">
+      <div class="overflow-hidden rounded-2xl bg-gray-200 shadow">
         <NuxtLink :to="`/store/${productAttr.slug}`">
           <img
             :src="imageURL"
             :alt="productAttr.name"
-            class="aspect-1 sm:aspect-2 md:aspect-1 object-cover object-center group-hover:opacity-75 w-full" />
+            class="aspect-1 sm:aspect-2 md:aspect-1 object-cover object-center group-hover:opacity-75 w-full shadow-black shadow-lg" />
         </NuxtLink>
       </div>
-      <div
-        class="absolute bottom-0 left-0 p-4 w-full rounded-b-2xl bg-gradient-to-t from-secondary-400 to-transparent drop-shadow-lg">
-        <h3
-          class="text-lg font-bold text-white line-clamp-1 drop-shadow-[0px_0px_2px_rgba(0,0,0,0.5)]">
-          {{ productAttr.name }}
-        </h3>
-        <p
-          class="mt-1 font-semibold text-white line-clamp-1 drop-shadow-[0px_0px_2px_rgba(0,0,0,0.5)]">
-          {{ $formatPrice(productAttr.price) }}
-        </p>
-      </div>
+    </div>
+    <div class="w-full rounded-b-2xl text-center">
+      <h3 class="text-lg font-bold text-gray-900 line-clamp-1">
+        {{ productAttr.name }}
+      </h3>
+      <p class="mt-1 font-semibold text-gray-600 line-clamp-1">
+        {{ $formatPrice(productAttr.price) }}
+      </p>
     </div>
     <NuxtLink
       :to="`/store/${productAttr.slug}`"
-      class="btn btn-primary rounded-lg text-secondary-900 bg-secondary-400 hover:bg-secondary-500 border-transparent hover:border-transparent px-5 font-bold w-full capitalize drop-shadow-lg">
+      class="btn btn-primary rounded-xl text-white text-base bg-secondary-500 hover:bg-secondary-600 border-transparent hover:border-transparent px-5 font-semibold w-full capitalize shadow">
       Beli Sekarang
     </NuxtLink>
   </div>
