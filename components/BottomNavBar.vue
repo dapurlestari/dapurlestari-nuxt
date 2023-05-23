b9c9e2eec673d500f8e851cb463ce8126dde09fb
Agil Setiawan <agilsetiawan772@gmail.com>
2023-05-23 19:37:44 +0700

    Update BottomNavBar.vue

--- components/BottomNavBar.vue
+++ components/BottomNavBar.vue
@@ -2,7 +2,7 @@
   <div class="relative">
     <!-- Bottom Navbar for mobile screen -->
     <div
-      class="fixed bottom-0 inline-flex md:hidden space-x-16 items-center justify-between px-8 py-3 bg-secondary-500 rounded-tl-3xl rounded-tr-3xl z-10">
+      class="fixed w-full bottom-0 inline-flex md:hidden space-x-16 items-center justify-center py-3 bg-secondary-500 rounded-tl-3xl rounded-tr-3xl z-10">
       <NuxtLink
         v-for="(menu, index) in $navMenu"
         :key="index"
